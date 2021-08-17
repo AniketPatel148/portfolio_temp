fcb48e3d3c841a68bd183d75e0a4ff518a7c7cc4
Aniket Patel <aniket.r.21102001@gmail.com>
2021-08-17 12:32:52 +0530

    Updates

---
+++ precache-manifest.1c50c87d0aec5f342da322301e69236b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f4c822cb253fa6886fb093c230ed29",
+    "url": "/portfolio-v1/index.html"
+  },
+  {
+    "revision": "39a2c2faa66dd5894fdf",
+    "url": "/portfolio-v1/static/css/main.48c3a4b1.chunk.css"
+  },
+  {
+    "revision": "6ed869bbc7220455ed8b",
+    "url": "/portfolio-v1/static/js/2.01e99f92.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio-v1/static/js/2.01e99f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a2c2faa66dd5894fdf",
+    "url": "/portfolio-v1/static/js/main.fd61b33b.chunk.js"
+  },
+  {
+    "revision": "0f87a7ced1099b429e32",
+    "url": "/portfolio-v1/static/js/runtime-main.b556fcf1.js"
+  },
+  {
+    "revision": "272265de524e4e3b4017c459d8de8cb4",
+    "url": "/portfolio-v1/static/media/Aniket_Resume.272265de.pdf"
+  },
+  {
+    "revision": "de32016c821eecf7b423d654648c58f5",
+    "url": "/portfolio-v1/static/media/amazonlite.de32016c.jpg"
+  },
+  {
+    "revision": "381af55d38e523ea981e73e577c6e608",
+    "url": "/portfolio-v1/static/media/bg-10.381af55d.png"
+  },
+  {
+    "revision": "d73e76cbbe3fb55faf0f6e186949792e",
+    "url": "/portfolio-v1/static/media/bg-2.d73e76cb.jpg"
+  },
+  {
+    "revision": "d4eb55f63542bd9d5339a579c295df7f",
+    "url": "/portfolio-v1/static/media/bg-6.d4eb55f6.jpg"
+  },
+  {
+    "revision": "c9266c61de871164617819b2ba3dc5e4",
+    "url": "/portfolio-v1/static/media/burger.c9266c61.jpg"
+  },
+  {
+    "revision": "f7ffffd184ba555071b35aefcd4a8a74",
+    "url": "/portfolio-v1/static/media/covid.f7ffffd1.jpg"
+  }
+]);
